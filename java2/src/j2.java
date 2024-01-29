e1c49dc3e7f18dc16352338a7bbacf766de1e3c4
HadisehIzadi <110168673+HadisehIzadi@users.noreply.github.com>
2024-01-29 20:13:33 +0330

    p1-p2

---
+++ java2/src/j2.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class j2 {
+    public static void main(String[] args)
+    {
+        int a = 20;
+        System.out.println("a = " + a);
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter w : ");
+        float w = scanner.nextFloat();
+
+        System.out.println("w : " + w);
+
+    }
+}
---
+++ java2/src/section2.java
@@ -0,0 +1,14 @@
+public class section2 {
+
+    public static void main(String[] args)
+    {
+        greeting();
+            }
+
+
+            static void greeting()
+
+            {
+                System.out.println("wellcom to this freaking language");
+            }
+}
